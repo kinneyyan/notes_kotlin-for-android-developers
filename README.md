c79a1fa96d0e8c78d9d3cde08502c9201a3edc2d
YanShi <shi.yan@ele.me>
2017-11-03 16:56:34 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # notes_kotlin-for-android-developers
 《kotlin-for-android-developers》的学习笔记和示例代码
 
-- [书籍下载：kotlin-for-android-developers-zh](https://github.com/kinneyyan/notes_kotlin-for-android-developers/raw/master/kotlin-for-android-developers-zh.epub)
+- 中文版下载：[kotlin-for-android-developers-zh](https://github.com/kinneyyan/notes_kotlin-for-android-developers/raw/master/kotlin-for-android-developers-zh.epub)
 
-- [学习笔记](https://github.com/kinneyyan/notes_kotlin-for-android-developers/blob/master/Kotlin%E5%AD%A6%E4%B9%A0%E7%AC%94%E8%AE%B0.md)
+- 笔记
+[1.类和函数](https://github.com/kinneyyan/notes_kotlin-for-android-developers/blob/master/notes/1.类和函数.md)
+[2.变量和属性](https://github.com/kinneyyan/notes_kotlin-for-android-developers/blob/master/notes/2.变量和属性.md)
+[3.数据类](https://github.com/kinneyyan/notes_kotlin-for-android-developers/blob/master/notes/3.数据类.md)
+[4.操作符重载](https://github.com/kinneyyan/notes_kotlin-for-android-developers/blob/master/notes/4.操作符重载.md)
+[5.Lambdas](https://github.com/kinneyyan/notes_kotlin-for-android-developers/blob/master/notes/5.Lambdas.md)
+
 
--- app/src/main/java/com/kingars/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/kingars/kotlindemo/MainActivity.kt
@@ -5,15 +5,20 @@ import android.os.Bundle
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.TypedValue
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import com.kingars.kotlindemo.domain.Forecast
 import com.kingars.kotlindemo.domain.ForecastList
 import com.kingars.kotlindemo.request.RequestForecastCommand
+import com.squareup.picasso.Picasso
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 /**
@@ -28,7 +33,6 @@ class MainActivity : AppCompatActivity() {
         val forecastList = findViewById<RecyclerView>(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
-//        forecastList.adapter = ForecastListAdapter(items)
 
         val requestBtn = findViewById<Button>(R.id.request_btn)
         val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
@@ -39,40 +43,55 @@ class MainActivity : AppCompatActivity() {
                 val result = RequestForecastCommand("94043").execute()
                 //如果调用者activity已被销毁，则uiThread内不会执行
                 uiThread {
-                    forecastList.adapter = ForecastListAdapter(result)
+                    //forecastList.adapter = ForecastListAdapter(result, { forecast -> toast(forecast.date) })
+                    //如果这个函数只接收一个参数，那我们可以使用it引用，而不用去指定左边的参数
+                    forecastList.adapter = ForecastListAdapter(result, { toast(it.date) })
                     progressBar.visibility = View.GONE
                 }
             }
         }
     }
 
-    class ForecastListAdapter(private val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    class ForecastListAdapter(private val weekForecast: ForecastList, private val itemClick: (Forecast) -> Unit) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
         override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-            val textView = TextView(parent?.context)
-            textView.setPadding(20, 20, 20, 20)
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16F)
-            return ViewHolder(textView)
+            val view = LayoutInflater.from(parent?.context).inflate(R.layout.item_forecast, parent, false)
+            return ViewHolder(view, itemClick)
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            with(weekForecast.dailyForecast[position]) {
-                holder.textView.text = "$date - $description - $high/$low"
+            holder.bindForecast(weekForecast[position])
+        }
+
+        override fun getItemCount(): Int = weekForecast.size()
+
+        class ViewHolder(view: View, private val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
+            private val iconView: ImageView
+            private val dateView: TextView
+            private val descriptionView: TextView
+            private val maxTemperatureView: TextView
+            private val minTemperatureView: TextView
+
+            init {
+                iconView = view.find(R.id.icon)
+                dateView = view.find(R.id.date)
+                descriptionView = view.find(R.id.description)
+                maxTemperatureView = view.find(R.id.maxTemperature)
+                minTemperatureView = view.find(R.id.minTemperature)
+            }
+
+            fun bindForecast(forecast: Forecast) {
+                with(forecast) {
+                    Picasso.with(itemView.context).load(iconUrl).into(iconView)
+                    dateView.text = date
+                    descriptionView.text = description
+                    maxTemperatureView.text = "$high"
+                    minTemperatureView.text = "$low"
+                    itemView.setOnClickListener { itemClick(forecast) }
+                }
             }
         }
-//        holder.textView.text = items[position]
 
-        override fun getItemCount(): Int = weekForecast.dailyForecast.size
-
-        class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
     }
 
-//    private val items = listOf(
-//            "Mon 6 / 23 - Sunny - 31 / 17",
-//            "Tue 6/24 - Foggy - 21/8",
-//            "Wed 6/25 - Cloudy - 22/17",
-//            "Thurs 6/26 - Rainy - 18/11",
-//            "Fri 6/27 - Foggy - 21/10",
-//            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-//            "Sun 6/29 - Sunny - 20/7"
-//    )
 }
